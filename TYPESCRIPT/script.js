d593e6a3b5ec7ac0022a1cc58a4123a1905de18a
krishna <krishnaptl0414@gmail.com>
2026-04-02 11:14:42 +0530

    Add script.js file

--- TYPESCRIPT/script.js
+++ TYPESCRIPT/script.js
@@ -1,13 +1,14 @@
 function add(a, b) {
     return a + b;
 }
+
 console.log(add(1, 2));
-console.log(add('Krishna', true));
-//JS Design-Loosely Types | Interpreted
+console.log(add('krishna', true));
 
 function printUser(userObj) {
-    console.log(userObj.fname);
+    console.log(userObj.fname[0]);
     console.log(userObj.lname);
-}
 
-printUser({});
+
+}
+printUser({})
